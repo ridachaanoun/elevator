500278ff7cf121f29a98a7b03fbfb74550ea213a
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2024-04-17 14:14:03 +0100

    first commit

---
+++ js/scr.js
@@ -0,0 +1,23 @@
+let elevatorDoor = document.querySelector(".elevator-door");
+let elevatorDoor2 = document.querySelector(".elevator-door2");
+
+
+
+
+
+
+
+
+
+
+
+
+function opening(){
+    elevatorDoor.style.transform = "translateX(-28px)";
+    
+}
+function opening2(){
+    elevatorDoor2.style.transform = "translateX(28px)";
+
+}
+
