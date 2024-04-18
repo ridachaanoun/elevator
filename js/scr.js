56116f61968e6e4ec24ea45a3f5e82538bedbf84
ridachaanoun <ridachaanoun.ff.2@gmail.com>
2024-04-18 12:56:02 +0100

    first commit

--- js/scr.js
+++ js/scr.js
@@ -1,23 +1,12 @@
 let elevatorDoor = document.querySelector(".elevator-door");
 let elevatorDoor2 = document.querySelector(".elevator-door2");
-
-
-
-
-
-
-
-
-
-
-
+let elevator = document.querySelector(".elevator-container .elevator");
+let start = document.querySelector(".start") ;
 
 function opening(){
     elevatorDoor.style.transform = "translateX(-28px)";
-    
-}
-function opening2(){
     elevatorDoor2.style.transform = "translateX(28px)";
-
 }
+start.addEventListener("click",opening);
+
 
